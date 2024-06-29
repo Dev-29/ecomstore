e870bdc328312e4e26c99b058a768b18cc40f91c
Dev Abhiram <devabhiram@gmail.com>
2024-06-30 00:49:05 +0530

    routes for updation and deletion

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,31 +1,56 @@
 const Product = require('../models/productModel');
 
-// exports.createProduct = async(req, res, next) => {
-//     const product = await Product.create(req.body);
-//     res.status(201).json({
-//         success: true,
-//         product
-//     })
-// }
-
+// Create product - Admin
 exports.createProduct = async (req, res, next) => {
-    console.log(req.body); // Log the request body
-    try {
-        const product = await Product.create(req.body);
-        res.status(201).json({
-            success: true,
-            product
-        });
-    } catch (error) {
-        console.error(error); // Log the error
-        res.status(400).json({
+    const product = await Product.create(req.body);
+    res.status(201).json({
+        success: true,
+        product
+    })
+}
+
+// Get all the products
+exports.getAllProducts = async (req, res) => {
+    const products = await Product.find()
+
+    res.status(200).json({
+        success: true,
+        products
+    })
+}
+
+// Update Product - Admin
+exports.updateProduct = async (req, res) => {
+    let product = Product.findById(req.params.id)
+
+    if (!product) {
+        return res.status(500).json({
             success: false,
-            message: error.message
-        });
+            message: 'Product not found'
+        })
     }
-};
 
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    })
+    res.status(200).json({
+        success: true,
+        product
+    })
+}
 
-exports.getAllProducts = (req, res) => {
-    res.status(200).json({message:"route working fine"})
+exports.deleteProduct = async (req, res, next) => {
+    const product = await Product.findById(req.params.id)
+    if (!product) {
+        return res.status(500).json({
+            success: false,
+            message: 'Product not found'
+        })
+    }
+
+    await product.deleteOne({ _id: req.params.id })
+
+    res.status(200).json({})
 }
